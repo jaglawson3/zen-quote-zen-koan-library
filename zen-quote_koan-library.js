1bcbbdc76f7844b75555a70a5d8ecb60489b3d6e
Justin Lawson <jaglawson3@gmail.com>
2015-11-12 12:20:12 -0700

    added title to koan objects

--- zen-quote_koan-library.js
+++ zen-quote_koan-library.js
@@ -11,11 +11,12 @@
     text:"The obstacle is the path."
     author:"- unknown"
   },{
-    text:""
-    author:""
+    text:"There is one thing: above, it supports Heaven; below, it upholds " +
+    "Earth. It is black like lacquer, always actively functioning."
+    author:"- Tung-shan Ling-chia"
   },{
-    text:""
-    author:""
+    text:" What is inexpressible is inexhaustible in its use."
+    author:"- Unknown Chinese Zen Master"
   },{
     text:""
     author:""
@@ -58,13 +59,37 @@
   }
   }],
   koans: [{
-    text: "When you can do nothing, what can you do?"
+    title:"Letter to a Dying Man"
+    text: "Bassui wrote the following letter to one of his disciples who was about to die:" +
+    "'The essence of your mind is not born, so it will never die. It is not an " +
+    "existence, which is perishable. It is not an emptiness, which is a mere void." +
+    " It has neither color nor form. It enjoys no pleasures and suffers no pains." +
+    "I know you are very ill. Liek a good Zen student, you are facing that sickness" +
+    " squarely. You may not know exactly who is suffering, but question yourself:" +
+    " What is the essence of this mind? Think only of this. You will need no more." +
+    " Covet nothing. Your end which is endless is as a snowflake dissolving in the pure air."
   },{
-    text: "What is your face before your mother and father were born?"
+    title:"Right and Wrong"
+    text: "When Bankei held his seclusion-weeks of meditation, pupils from many"+
+    " parts of Japan came to attend. During one of these gatherings a pupil was" +
+    " caught stealing. The matter was reported to Bankei with the request that" +
+    " the culprit be expelled. Bankei ignored the case. Later the pupil was " +
+    "caught in a similar act, and again bankei disregarded the matter. this " +
+    "angered the other pupils, who drew up a petition asking for the dismissal " +
+    "of the thief, stating that otherwise they woudl leave in a body." +
+    "When bankei had read the petition he called everyone before him. 'You are" +
+    " wise brothers,' he told them. 'You know what is right and what is not " +
+    "right. You may somewhere else to study if ou wish, but this poor brother " +
+    "does not even know right from wrong. Who will teach him if I do not? I am " +
+    "going to keep him here even if all the rest of you leave.' A torrent of " +
+    "tears cleansed the face of the brother who had stolen. All desire to steal" +
+    " had vanished."
   },{
+    title:
     text: "When an ordinary man attains knowledge, he is a sage; when a sage " +
     "attains understanding, he is an ordinary man."
   },{
+    title:
     text:""
   }]
 }
