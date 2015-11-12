993543eb183a7b37b963e958672b67eda561a90d
Justin Lawson <jaglawson3@gmail.com>
2015-11-12 11:54:44 -0700

    added concat to strings in JSON objects

--- zen-quote_koan-library.js
+++ zen-quote_koan-library.js
@@ -2,14 +2,14 @@
   quotes: [{
   },{
     text:"He who knows, does not speak. He who speaks, does not know."
-    author: "Lao Tzu"
+    author: "- Lao Tzu"
   },{
-    text:"Children are natural Zen masters; their world is brand new in each \
-    and every moment."
-    author:"John Bradshaw"
+    text:"Children are natural Zen masters; their world is brand new in each " +
+    "and every moment."
+    author:"- John Bradshaw"
   },{
     text:"The obstacle is the path."
-    author:"unknown"
+    author:"- unknown"
   },{
     text:""
     author:""
@@ -24,37 +24,37 @@
     author:""
   },{
     text: "You are that vast thing you see far, far off with great telescopes.",
-    author: "Alan Watts"
+    author: "- Alan Watts"
   },{
-    text: "Zen opens a man's eyes to the greatest mystery as it is daily and,\
-     hourly performed; it enlarges the heart to embrace eternity of time and \
-     infinity of space in its every palpitation; it makes us live in the world \
-     as if walking in the garden of Eden",
-    author: "D.T. Suzuki"
+    text: "Zen opens a man's eyes to the greatest mystery as it is daily and," +
+     "hourly performed; it enlarges the heart to embrace eternity of time and " +
+     "infinity of space in its every palpitation; it makes us live in the world " +
+     "as if walking in the garden of Eden", +
+    author: "- D.T. Suzuki"
   },{
-    text: "Zen is not some kind of excitement, but concentration on our usual \
-    everyday routine.",
-    author: "Shunryu Suzuki"
+    text: "Zen is not some kind of excitement, but concentration on our usual " +
+    "everyday routine.", +
+    author: "- Shunryu Suzuki"
   },{
-    text: "For Zen, man is the goal; man is the end unto himself. God is not \
-    something above humanity, God is something hidden within humanity. Man is \
-    carrying God in himself as a potentiality.",
-    author: "Osho"
+    text: "For Zen, man is the goal; man is the end unto himself. God is not " +
+    "something above humanity, God is something hidden within humanity. Man is " +
+    "carrying God in himself as a potentiality.",
+    author: "- Osho"
   },{
-    text: "You should study not only that you become a mother when your child \
-    is born, but also that you become a child.",
-    author: "Dogen Zenji"
+    text: "You should study not only that you become a mother when your child " +
+    "is born, but also that you become a child.",
+    author: "- Dogen Zenji"
   },{
-    text: "If you're afraid of being grabbed by God, don't look at a wall.\
-    Definitely don't sit still.",
-    author: "Jiyu Kennet"
+    text: "If you're afraid of being grabbed by God, don't look at a wall. " +
+    "Definitely don't sit still.",
+    author: "- Jiyu Kennet"
   },{
-    text: "The fundamental delusion of humanity is to suppose that I am here \
-    and you are out there. ",
-    author: "Yasutani Roshi"
+    text: "The fundamental delusion of humanity is to suppose that I am here " +
+    "and you are out there.",
+    author: "- Yasutani Roshi"
   },{
-    text: "Water which is too pure has no fish. ",
-    author: "Hong Zichen"
+    text: "Water which is too pure has no fish.",
+    author: "- Hong Zichen"
   }
   }],
   koans: [{
@@ -62,11 +62,9 @@
   },{
     text: "What is your face before your mother and father were born?"
   },{
-    text: "When an ordinary man attains knowledge, he is a sage; when a sage \
-    attains understanding, he is an ordinary man."
+    text: "When an ordinary man attains knowledge, he is a sage; when a sage " +
+    "attains understanding, he is an ordinary man."
   },{
     text:""
   }]
 }
-
-console.log()
